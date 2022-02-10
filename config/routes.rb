72c2dc6b4ad7e22892d6d708e349727fb98b395f
Smail Liams <b.smailiams@gmail.com>
2022-02-10 12:40:27 +0100

    Add Users using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :us
   resources :messages
   root 'messages#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_093951) do
+ActiveRecord::Schema.define(version: 2022_02_10_113128) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "us", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_us_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_us_on_reset_password_token", unique: true
   end
 
 end
